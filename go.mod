a8a998d06da89f1903127d44dfa87ad90dd39926
A.Unger <zyxancf@gmail.com>
2021-05-25 14:32:35 +0200

    game of life

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gogol
+
+go 1.16
+
+require github.com/jinzhu/copier v0.3.0 // indirect
---
+++ main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestBoardState(t *testing.T) {
+	//b := newBoard(16, 16)
+}
